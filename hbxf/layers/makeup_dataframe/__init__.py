0cb18f14384f886e19977ceae06500376a646ab8
sunxiuqi <16600296641@163.com>
2020-08-25 16:15:20 +0800

    name and stack limit

--- hbxf/layers/makeup_dataframe/__init__.py
+++ hbxf/layers/makeup_dataframe/__init__.py
@@ -1,6 +1,6 @@
 from layers.makeup_dataframe.add_ceil import add_ceil
 from layers.makeup_dataframe.merge_initialized_table import merge_initialized_table
-from layers.makeup_dataframe.order_limit import order_limit
+from layers.makeup_dataframe.order_limit import order_limit, name_stack_limit
 from layers.makeup_dataframe.add_unit import add_unit
 
 
@@ -13,6 +13,10 @@ def makeup_dataframe(dataframe_list):
     """
 
     for dataframe in dataframe_list:
+
+        name = dataframe.get("name")
+        stack = dataframe.get("stack")
+        value = dataframe.get("value")
         # 排序补零：
         if dataframe.get("full"):
             df = merge_initialized_table(dataframe)  # 融合数据表：加 处理了day的初始化问题
@@ -21,6 +25,16 @@ def makeup_dataframe(dataframe_list):
         # 重新排序+limit
         if dataframe.get("order") or dataframe.get("limit"):
             df = order_limit(dataframe)
+            dataframe["df"] = df
+
+        # name_limit
+        if dataframe.get("name_limit"):
+            df = name_stack_limit(dataframe, "name_limit", name, value)
+            dataframe["df"] = df
+
+        # stack_limit
+        if dataframe.get("stack_limit"):
+            df = name_stack_limit(dataframe, "stack_limit", stack, value)
             dataframe["df"] = df
 
         # 添加单位
