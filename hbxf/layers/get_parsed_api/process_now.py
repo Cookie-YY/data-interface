88f52f5e834b95d2c2c64fe34af81b72914f9b58
sunxiuqi <16600296641@163.com>
2020-09-10 11:27:46 +0800

    增加day_now(now-30,now+7)

--- hbxf/layers/get_parsed_api/process_now.py
+++ hbxf/layers/get_parsed_api/process_now.py
@@ -22,18 +22,29 @@ def process_now(api_dict):
             value = value.replace("now", str(datetime.now().month))
 
         elif k == "day":
-            res = re.search('(?:\+|-)\d*', v)
-            if res:
-                update_num = res.group()
-                now_time = datetime.now().strftime("%Y-%m-%d")  # 现在时间
-                now_time_str = datetime.strptime(now_time, "%Y-%m-%d")  # 转化为字符串
+            now_time = datetime.now().strftime("%Y-%m-%d")  # 现在时间
+            now_time_str = datetime.strptime(now_time, "%Y-%m-%d")  # 转化为字符串
+            res = re.findall('(?:\s|-)\d+', v)   #  ['-20', ' 30']  ['7']  ['-30']
+            if len(res) == 2:
+                num0 = res[0]
+                num1 = res[1]
+                num0_time0 = timedelta(days=int(num0))
+                num0_time1 = timedelta(days=int(num1))
+                update_time0 = (now_time_str + num0_time0).strftime("%Y-%m-%d")
+                update_time1 = (now_time_str + num0_time1).strftime("%Y-%m-%d")
+                if int(num0) > int(num1):
+                    value = update_time1 + "," + update_time0
+                else:
+                    value = update_time0 + "," + update_time1
+            else:
+                update_num = res[0]
                 update_days = timedelta(days=int(update_num))
                 update_time = (now_time_str+update_days).strftime("%Y-%m-%d")  # str
-                if "-" in v:
+                if "-" in update_num:
                     value = update_time + "," + now_time
-                if "+" in v:
+                else:
                     value = now_time + "," + update_time
-                update_dict[k] = value
+            update_dict[k] = value
 
         if value.startswith("[") and value.endswith("]"):
             min_value = value.strip("[").strip("]").split(",")[0]  # "2020-1"
@@ -47,6 +58,7 @@ def process_now(api_dict):
             value_parsed = f"{result1},{result2}"
             update_dict[k] = value_parsed
     api_dict.update(update_dict)
+    print(api_dict)
     return 200, "success", api_dict
 
 
