5aa09e53e41b2c612e2ef061357a0d72d52d9353
sunxiuqi <16600296641@163.com>
2020-09-18 16:08:52 +0800

    修改hb重点人员url添加wxzd

--- hbxf/settings/hbxf/apis_dispatch.py
+++ hbxf/settings/hbxf/apis_dispatch.py
@@ -31,12 +31,12 @@ URL_DISPATCH_MAP = {
 
     # 重点人员姓名列表
     "信访人分析_重点人员_全部":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xm_xfjc&name=xm&value=zjhm&direct_order=-xfjc&direct_limit=100",
-    "信访人分析_重点人员_来信":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xfxs_xfjc&name=xm&value=zjhm&direct_order=-xfjc&xfxs=来信&direct_limit=100",
-    "信访人分析_重点人员_来访":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xfxs_xfjc&name=xm&value=zjhm&direct_order=-xfjc&xfxs=来访&direct_limit=100",
-    "信访人分析_重点人员_网信":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xfxs_xfjc&name=xm&value=zjhm&direct_order=-xfjc&xfxs=网信&direct_limit=100",
+    "信访人分析_重点人员_来信":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xm_xfxs_xfjc&name=xm&value=zjhm&direct_order=-xfjc&xfxs=来信&direct_limit=100",
+    "信访人分析_重点人员_来访":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xm_xfxs_xfjc&name=xm&value=zjhm&direct_order=-xfjc&xfxs=来访&direct_limit=100",
+    "信访人分析_重点人员_网信":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xm_xfxs_xfjc&name=xm&value=zjhm&direct_order=-xfjc&xfxs=网信&direct_limit=100",
     # 暂时没有扬言的数据表
-    "信访人分析_重点人员_扬言":"",
-    "信访人分析_重点人员_专项":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_zxbz_xfjc&name=xm&value=zjhm&direct_order=-xfjc&direct_limit=100",
+    "信访人分析_重点人员_扬言":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xm_yy_xfjc&name=xm&value=zjhm&direct_order=-xfjc&direct_limit=100",
+    "信访人分析_重点人员_专项":"http://39.107.240.28:3389/api/xf/?table=xf_xfr_cy_zjhm_xm_zxbz_xfjc&name=xm&value=zjhm&direct_order=-xfjc&direct_limit=100",
 
     # 攻坚治理_地图
     "攻坚治理_重点人员_全部_件次":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_xfjc_zb&name=shij_02&value=xfjc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
@@ -44,7 +44,7 @@ URL_DISPATCH_MAP = {
     "攻坚治理_重点人员_赴省访_件次":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_fsf_xfjc_zb&name=shij_02&value=xfjc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
     "攻坚治理_重点人员_全部_人次_问题属地":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_wtsdrc_zb&name=shij_02&value=wtsdrc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
     "攻坚治理_重点人员_进京访_人次_问题属地":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_jjf_wtsdrc_zb&name=shij_02&value=wtsdrc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
-    "攻坚治理_重点人员_赴省访_人次_问题属地":"没有表",
+    "攻坚治理_重点人员_赴省访_人次_问题属地":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_fsf_wtsdrc_zb&name=shij_02&value=wtsdrc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
     "攻坚治理_重点人员_全部_人次_常住地":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_czdrc_zb&name=shij_02&value=czdrc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
     "攻坚治理_重点人员_进京访_人次_常住地":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_jjf_czdrc_zb&name=shij_02&value=czdrc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid",
     "攻坚治理_重点人员_赴省访_人次_常住地":"http://39.107.240.28:3389/api/xf/?table=xf_xfj_cd_xj_zd_fsf_czdrc_zb&name=shij_02&value=czdrc&transformer=@groupby&day=[2020-01-01,2020-05-30]&shij_02=invalid"
